0afa4c6b294e4e873bb04055caa08b0097744e11
bm1478 <bm1478@naver.com>
2020-03-30 10:53:29 +0900

    update truffle network setting

--- Ethereum/deploy-contract/truffle-config.js
+++ Ethereum/deploy-contract/truffle-config.js
@@ -20,29 +20,23 @@
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-const result = JSON.parse(fs.readFileSync('/../ropstenAPIKey.json', 'utf8'));
-const ropstenAPIKey = result["key"];
+const apiResult = JSON.parse(fs.readFileSync('../ropstenAPIKey.json', 'utf8'));
+const mnemonicResult = JSON.parse(fs.readFileSync("../ropstenMnemonic.json", 'utf8'));
+const ropstenAPIKey = apiResult["key"];
+const mnemonic = mnemonicResult["mnemonic"];
 
 module.exports = {
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
     ropsten: {
-      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/`+ropstenAPIKey),
+      provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/` + ropstenAPIKey),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
-    
-    },
+  },
   compilers: {
     solc: {
       version: "^0.4.0",
@@ -53,8 +47,8 @@ module.exports = {
           enabled: false,
           runs: 200
         },
-        evmVersion: "byzantium"
+        evmVersion: "petersburg"
       }
     }
   }
-}
+};
